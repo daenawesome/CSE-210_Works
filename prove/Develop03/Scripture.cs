d4d84ac9038bf47b58a8f047cb35219ca393e2a3
Daen Antule <ant21006@byui.edu>
2023-05-05 02:32:30 +0300

    Add Load txt file, diffi, shuff and comm. No Valid

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,49 +3,66 @@ using System.Collections.Generic;
 
 public class Scripture
 {
-    private List<Verse> verses;
+    // private fields
+    private string reference; // a string that represents the reference of the scripture
+    private List<Verse> verses; // a list of Verse objects that represents the verses of the scripture
 
-    public Scripture(string reference, string text)
+    // public properties
+    public List<Verse> GetVerses() // returns the list of Verse objects
     {
-        verses = new List<Verse>();
-        verses.Add(new Verse(reference, text));
+        return verses;
     }
 
-    public Scripture(string reference, string[] texts)
+    public string GetReference() // returns the reference of the scripture
     {
-        verses = new List<Verse>();
-        for (int i = 0; i < texts.Length; i++)
+        return reference;
+    }
+
+    // constructors
+    public Scripture(string reference, string text) // constructor that takes a reference and a text
+    {
+        this.reference = reference; // sets the reference field to the given reference
+        verses = new List<Verse>(); // initializes the verses list
+        verses.Add(new Verse(reference, text)); // creates a new Verse object with the given reference and text, and adds it to the verses list
+    }
+
+    public Scripture(string reference, string[] texts) // constructor that takes a reference and an array of texts
+    {
+        this.reference = reference; // sets the reference field to the given reference
+        verses = new List<Verse>(); // initializes the verses list
+        for (int i = 0; i < texts.Length; i++) // iterates through each text in the texts array
         {
-            verses.Add(new Verse(reference + "-" + (i + 1), texts[i]));
+            verses.Add(new Verse(reference, texts[i])); // creates a new Verse object with the given reference and text, and adds it to the verses list
         }
     }
 
-    public void HideRandomWord()
+    // methods
+    public void HideRandomWords(int numWordsToHide) // method that takes a number of words to hide, and hides that many words in each verse of the scripture
     {
-        foreach (Verse verse in verses)
+        foreach (Verse verse in verses) // iterates through each verse in the verses list
         {
-            verse.HideRandomWord();
+            verse.HideRandomWords(numWordsToHide); // calls the HideRandomWords method of the Verse object, passing in the numWordsToHide parameter
         }
     }
 
-    public bool IsFullyHidden()
+    public bool IsFullyHidden() // method that checks if all the words in all the verses of the scripture are hidden
     {
-        foreach (Verse verse in verses)
+        foreach (Verse verse in verses) // iterates through each verse in the verses list
         {
-            if (!verse.IsFullyHidden())
+            if (!verse.IsFullyHidden()) // if the verse is not fully hidden
             {
-                return false;
+                return false; // return false
             }
         }
-        return true;
+        return true; // if all the verses are fully hidden, return true
     }
 
-    public void Print()
+    public void Print() // method that clears the console and prints each verse of the scripture
     {
-        Console.Clear();
-        foreach (Verse verse in verses)
+        Console.Clear(); // clears the console
+        foreach (Verse verse in verses) // iterates through each verse in the verses list
         {
-            Console.WriteLine(verse.ToString());
+            Console.WriteLine(verse.ToString()); // calls the ToString method of the Verse object, and writes the result to the console
         }
     }
 }
--- prove/Develop03/Verse.cs
+++ prove/Develop03/Verse.cs
@@ -3,55 +3,70 @@ using System.Collections.Generic;
 
 public class Verse
 {
-    private string reference;
-    private List<Word> words;
+    private string reference; // The reference of the verse, e.g. "John 3:16"
+    private List<Word> words; // A list of words in the verse
 
     public Verse(string reference, string text)
     {
         this.reference = reference;
-        words = new List<Word>();
-        foreach (string word in text.Split(' '))
+        words = new List<Word>(); // Initialize an empty list of words
+        foreach (string word in text.Split(' ')) // Split the input text into words
         {
-            words.Add(new Word(word));
+            words.Add(new Word(word)); // Add each word to the list of words
         }
     }
 
-    public void HideRandomWord()
+    public void HideRandomWords(int numWordsToHide)
     {
-        List<int> visibleIndices = new List<int>();
+        List<int> visibleIndices = new List<int>(); // Initialize a list of visible word indices
         for (int i = 0; i < words.Count; i++)
         {
-            if (!words[i].IsHidden())
+            if (!words[i].IsHidden()) // If the word is not already hidden
             {
-                visibleIndices.Add(i);
+                visibleIndices.Add(i); // Add the index of the word to the list of visible indices
             }
         }
-        if (visibleIndices.Count > 0)
+        if (visibleIndices.Count > 0) // If there are visible words to hide
         {
-            int indexToHide = visibleIndices[new Random().Next(visibleIndices.Count)];
-            words[indexToHide].Hide();
+            Random random = new Random(); // Initialize a random number generator
+            for (int i = 0; i < numWordsToHide; i++)
+            {
+                int indexToHide = visibleIndices[random.Next(visibleIndices.Count)]; // Choose a random visible index to hide
+                words[indexToHide].Hide(); // Hide the word at the chosen index
+                visibleIndices.Remove(indexToHide); // Remove the hidden word index from the list of visible indices
+                if (visibleIndices.Count == 0) // If there are no more visible words
+                {
+                    break; // Exit the loop
+                }
+            }
         }
     }
 
     public bool IsFullyHidden()
     {
-        foreach (Word word in words)
+        foreach (Word word in words) // Loop through all words in the verse
         {
-            if (!word.IsHidden())
+            if (!word.IsHidden()) // If a word is not hidden
             {
-                return false;
+                return false; // The verse is not fully hidden
             }
         }
-        return true;
+        return true; // All words are hidden, so the verse is fully hidden
     }
 
     public override string ToString()
     {
-        string output = reference + ": ";
-        foreach (Word word in words)
+        string output = reference + ": "; // Start the output string with the reference
+        foreach (Word word in words) // Loop through all words in the verse
         {
-            output += word.ToString() + " ";
+            output += word.ToString() + " "; // Add the string representation of the word to the output string, followed by a space
         }
-        return output;
+        return output; // Return the output string
+    }
+
+    public void ShuffleWords()
+    {
+        Random random = new Random(); // Initialize a random number generator
+        words = words.OrderBy(x => random.Next()).ToList(); // Shuffle the list of words using the random number generator
     }
 }
